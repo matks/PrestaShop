fc15c21bfa0b347c19269074301530a18fa073b7
matks <mathieu.ferment@prestashop.com>
2020-12-01 17:36:54 +0100

    3

--- tests/check_sanity.sh
+++ tests/check_sanity.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd tests/UI
-docker-compose -f docker-compose.nightly.yml -f docker-compose.tests.yml up --build
+docker-compose -f docker-compose.nightly.yml -f docker-compose.tests.yml up --build -d
 docker-compose -f docker-compose.nightly.yml -f docker-compose.tests.yml exec -e COMMAND="sanity-tests" tests bash /tmp/run-tests.sh
